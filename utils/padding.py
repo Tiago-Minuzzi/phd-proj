b7c24ddb2bb56d85cbcab25f7d102168d27df926
Tiago-Minuzzi <tiago.minuzzi87@google.com>
2020-10-26 15:51:50 -0300

    Padding as function

--- utils/padding.py
+++ utils/padding.py
@@ -2,14 +2,19 @@
 # Pad sequences
 import sys
 in_fasta = sys.argv[1]
+out_fasta = sys.argv[2]
+
+def padding(in_fasta, out_fasta):
 # Pad sequences to the leght of the longest sequence
-maxLen=len(max(open(in_fasta),key=len).strip())
-# Pad to the end
-with open(in_fasta, 'r') as fasta, open("pad_out.fasta","w") as pout:
-    for line in fasta:
-        line = line.strip()
-        if not line.startswith('>'):
-            line = line.upper().ljust(maxLen, 'P') + 'TTTAAAGGGCCCNNN'
-        pout.write(line+"\n")
-        print(line)
-    print(f'Longest sequence is {maxLen} nucleotides.')
+    maxLen=len(max(open(in_fasta),key=len).strip())
+    # Pad to the end
+    with open(in_fasta, 'r') as fasta, open(out_fasta,"w") as pout:
+        for line in fasta:
+            line = line.strip()
+            if not line.startswith('>'):
+                line = line.upper().ljust(maxLen, 'P') + 'TTTAAAGGGCCCNNN'
+            pout.write(line+"\n")
+            # print(line)
+        print(f'Longest sequence is {maxLen} nucleotides.')
+
+padding(in_fasta, out_fasta)
