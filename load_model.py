fd2b851cf7e87770137f50568d66fecfd4111112
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2020-11-04 11:40:43 -0300

    Try/except block in load_model and nadd models

--- load_model.py
+++ load_model.py
@@ -12,6 +12,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
 # Hide warning messages
+from tensorflow.python.util import deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
 simplefilter(action='ignore', category=FutureWarning)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 with redirect_stderr(open(os.devnull, "w")):
@@ -26,12 +28,12 @@ np.set_printoptions(threshold=sys.maxsize) # Show full arrays (without truncatio
 inp_arq = sys.argv[1]
 out_arq = sys.argv[2] if len(sys.argv) >= 3 else None # Check whether the element exists
 pad_ref = "fastas/pad_ref03.fasta"
-tes_mdl = "models/model_var01.05.hdf5"
-#tes_mdl = "models/model_var01.05-01.hdf5"
+tes_mdl = "models/model_var01.05.wb.hdf5"
+
 
 # Pre-processing functions
 # Fasta to data frame
-def fasta_frame(fasta_file,label):
+def fasta_frame(fasta_file, label):
   identifiers = []
   sequences = []
   with open(fasta_file) as f_f:
@@ -69,46 +71,50 @@ def flatten_sequence(pred_fasta_flat):
   pred_fasta_flat=pred_fasta_flat.reshape(n_samples,n_xy)
   return(pred_fasta_flat)
 
-# Input pre-processing
-user_frame = fasta_frame(inp_arq,'?')
-pad_frame = fasta_frame(pad_ref,'?')
-u_f = [user_frame,pad_frame]
-user_df = pd.concat(u_f,ignore_index=True)
 
-# One hot encode and flat sequences
-user_df_ohe = ohe_fun(user_df)
-user_df_flat=flatten_sequence(user_df_ohe)
+try:
+	# Input pre-processing
+	user_frame = fasta_frame(inp_arq,'?')
+	pad_frame = fasta_frame(pad_ref,'?')
+	u_f = [user_frame,pad_frame]
+	user_df = pd.concat(u_f,ignore_index=True)
 
-# Remove pad_ref
-user_seq_to_pred = np.delete(user_df_flat, -1, 0)
+	# One hot encode and flat sequences
+	user_df_ohe = ohe_fun(user_df)
+	user_df_flat=flatten_sequence(user_df_ohe)
 
-# Load CNN model and predict
-modelo = load_model(tes_mdl)
-res_prob = modelo.predict_proba(np.expand_dims(user_seq_to_pred, axis=2), batch_size = 2)
-res_label = modelo.predict_classes(np.expand_dims(user_seq_to_pred, axis=2), batch_size = 2)
-print("There is an invalid sequence")
+	# Remove pad_ref
+	user_seq_to_pred = np.delete(user_df_flat, -1, 0)
 
-# Format results
-pd.set_option('display.float_format', lambda x: f'{x:.3f}')
-res_label_S = pd.Series(res_label)
-res_prob_0 = pd.Series((res_prob[:,0])*100)
-res_prob_1 = pd.Series((res_prob[:,1])*100)
-user_ids = pd.Series(user_df['ID'][:-1])
-results_dict = { 'ID': user_ids, 'Not TE prob': res_prob_0, 'TE prob': res_prob_1, 'Classification': res_label_S } 
-results_df = pd.DataFrame(results_dict)
-results_df.set_index('ID',inplace=True)
-results_df['Classification'].replace(0,'NT',inplace=True)
-results_df['Classification'].replace(1,'TE',inplace=True)
+	# Load CNN model and predict
+	modelo = load_model(tes_mdl)
+	res_prob = modelo.predict_proba(np.expand_dims(user_seq_to_pred, axis=2), batch_size = 2)
+	res_label = modelo.predict_classes(np.expand_dims(user_seq_to_pred, axis=2), batch_size = 2)
 
-# Show prediction results
-print("\n")
-print("# RESULTS")
-print(results_df)
-print("\n")
+	# Format results
+	pd.set_option('display.float_format', lambda x: f'{x:.3f}')
+	res_label_S = pd.Series(res_label)
+	res_prob_0 = pd.Series((res_prob[:,0])*100)
+	res_prob_1 = pd.Series((res_prob[:,1])*100)
+	user_ids = pd.Series(user_df['ID'][:-1])
+	results_dict = { 'ID': user_ids, 'Not TE prob': res_prob_0, 'TE prob': res_prob_1, 'Classification': res_label_S } 
+	results_df = pd.DataFrame(results_dict)
+	results_df.set_index('ID',inplace=True)
+	results_df['Classification'].replace(0,'NT',inplace=True)
+	results_df['Classification'].replace(1,'TE',inplace=True)
 
-# Write to file and show message
-if out_arq:
-    results_df.to_csv(out_arq)
-    print(f"File saved as {out_arq}")
-else:
-    print("No output file created")
+	# Show prediction results
+	print("\n")
+	print("# RESULTS")
+	print(results_df)
+	print("\n")
+
+	# Write to file and show message
+	if out_arq:
+	    results_df.to_csv(out_arq)
+	    print(f"File saved as {out_arq}")
+	else:
+	    print("No output file created")
+
+except ValueError:
+	print(f"\nThere is an invalid sequece in '{inp_arq}'.\n")
