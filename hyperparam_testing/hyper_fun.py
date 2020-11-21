343568e6686bec2880beb950d0efa4ec6d95fad7
Tiago-Minuzzi <tiago.minuzzi87@google.com>
2020-11-21 08:50:57 -0300

    Hyperparameter testing scripting improvements

--- hyperparam_testing/hyper_fun.py
+++ hyperparam_testing/hyper_fun.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
-# Imports
 import os
-import talos as ta
 import pandas as pd
-from Bio import SeqIO
-from warnings import simplefilter
 import numpy as np
 from numpy import array
-from numpy import argmax
-from sklearn import preprocessing
+from Bio import SeqIO
+from warnings import simplefilter
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
 import tensorflow
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv1D, Dense, MaxPooling1D, Flatten
-from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
+# Suppress warnings etc
 simplefilter(action = 'ignore', category = FutureWarning)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['AUTOGRAPH_VERBOSITY'] = '0'
-# Files
-dataset = 'nadd_ml_df_train_set_00.01.csv'
-# Set seed for model reproducibility
-SEED = 13
+
+
 # Functions
 def fasta_frame(fasta_file,label):
   identifiers = []
@@ -78,57 +71,10 @@ def te_hype(x_train, y_train, x_val, y_val, params):
 	model.compile(loss="categorical_crossentropy",
 	              optimizer='adam',
 	              metrics=['acc'])
-	history = model.fit(x_train_3d, ynn_train, 
+	history = model.fit(x_train, y_train, 
 	                  epochs=params['epochs'],
 	                  batch_size=params['batch_size'],
 	                  verbose=1,
 	                  validation_split=0.0,
-	                  validation_data=(x_test_3d,ynn_test))
-	
+	                  validation_data=(x_val, y_val))	
 	return(history, model)
-
-
-# SEED for model reproducibility
-np.random.seed(SEED)
-if tensorflow.__version__ == '2.3.1':
-  tensorflow.random.set_seed(SEED)
-else:
-  #tensorflow.random.set_random_seed(SEED)
-  tensorflow.compat.v1.random.set_random_seed(SEED)
-# Import from csv
-seq_df = pd.read_csv(dataset)
-# Preprocessing
-## Sequences
-x_sequence_arrays = ohe_fun(seq_df)
-x_flat_2d = flatten_sequence(x_sequence_arrays)
-## Labels
-y_str=seq_df['label']
-lbenc = preprocessing.LabelBinarizer()
-ynn = lbenc.fit_transform(y_str)
-encoded = to_categorical(ynn)
-# Train and test splits
-x_train, x_test, ynn_train, ynn_test = train_test_split(x_flat_2d, 
-                                                    encoded,
-                                                    test_size = 0.20, 
-                                                    random_state = SEED,
-                                                    stratify = y_str)
-# Expand dimensions
-x_train_3d = np.expand_dims(x_train, axis=2)
-x_test_3d = np.expand_dims(x_test, axis=2)
-# Hyperparameter testing
-p = {'lr': (0.5, 5, 10),
-     'first_neuron': [32],
-     'second_neuron': [24],
-     'third_neuron': [24],
-     'dense01_neuron': [24],
-     'kernel_size': [7,8,9,12],
-     'pool_size': [3,4,6,7],
-     'batch_size': [10,25],
-     'epochs': [15,20]}
-# Run scanning
-teste = ta.Scan(x=x_train_3d,
-            y=ynn,
-            model=te_hype,
-            fraction_limit=0.01, 
-            params=p,
-            experiment_name='te_identification')
---
+++ hyperparam_testing/hyper_testing.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# Imports
+import os
+import talos as ta
+import pandas as pd
+import numpy as np
+from warnings import simplefilter
+import tensorflow
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from hyper_fun import fasta_frame, ohe_fun, flatten_sequence, te_hype
+# Suppress warnings etc
+simplefilter(action = 'ignore', category = FutureWarning)
+os.environ['AUTOGRAPH_VERBOSITY'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# Files
+dataset = f'../tables_etc/ml_df_train_set_00.01.csv'
+# Hyperparameter settings
+experiment_name = 'te_identification'
+parameters = {'lr': (0.5, 5, 10),
+     'first_neuron': [32],
+     'second_neuron': [24],
+     'third_neuron': [24],
+     'dense01_neuron': [24],
+     'kernel_size': [7,8,9,12],
+     'pool_size': [3,4,6,7],
+     'batch_size': [10,25],
+     'epochs': [15,20]}
+# Set seed for model reproducibility
+SEED = 13
+np.random.seed(SEED)
+if tensorflow.__version__ == '2.3.1':
+  tensorflow.random.set_seed(SEED)
+else:
+  #tensorflow.random.set_random_seed(SEED)
+  tensorflow.compat.v1.random.set_random_seed(SEED)
+# Import from csv
+seq_df = pd.read_csv(dataset)
+# Preprocessing
+## Sequences
+x_sequence_arrays = ohe_fun(seq_df)
+x_flat_2d = flatten_sequence(x_sequence_arrays)
+## Labels
+y_str=seq_df['label']
+lbenc = preprocessing.LabelBinarizer()
+ynn = lbenc.fit_transform(y_str)
+encoded = to_categorical(ynn)
+# Train and test splits
+x_train, x_test, ynn_train, ynn_test = train_test_split(x_flat_2d, 
+                                                    encoded,
+                                                    test_size = 0.20, 
+                                                    random_state = SEED,
+                                                    stratify = y_str)
+# Expand dimensions
+x_train_3d = np.expand_dims(x_train, axis=2)
+x_test_3d = np.expand_dims(x_test, axis=2)
+# Run scanning
+teste = ta.Scan(x=x_train_3d,
+                y=ynn_train,
+                x_val=x_test_3d,
+                y_val=ynn_test,
+                model=te_hype,
+                fraction_limit=0.01, 
+                params=parameters,
+                experiment_name=experiment_name)
